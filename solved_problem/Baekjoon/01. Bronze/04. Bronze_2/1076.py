043c2ba51a0213c9d7bbfd0c8e8048a5c8b34a19
seola1ne <seolalny@gmail.com>
2022-12-26 10:14:16 +0900

    ADD :: 1076번 저항 주석 추가

--- solved_problem/Baekjoon/01. Bronze/04. Bronze_2/1076.py
+++ solved_problem/Baekjoon/01. Bronze/04. Bronze_2/1076.py
@@ -1,4 +1,4 @@
-def switch(key):
+def switch(key): # 색상에 따른 값을 구하는 함수
     rs = {"black" : 0,
         "brown" : 1,
         "red" : 2,
@@ -12,19 +12,18 @@ def switch(key):
         }.get(key, "")
     return rs
 
-def square(n):
+def square(n): # 세 번째 수의 값에 따라 10의 제곱 수를 구하는 함수
     res = 1
     for i in range(n):
         res *= 10
     return res
     
+r1 = input() # 첫 번째 저항 값 입력
+r2 = input() # 두 번째 저항 값 입력
+mul = input() # 세 번째, 곱하는 값 입력
 
-r1 = input()
-r2 = input()
-r3 = input()
-
-r1 = switch(r1)*10
+r1 = switch(r1)*10 # 
 r2 = switch(r2)
-mul = square(switch(r3))
+mul = square(switch(mul)) # 곱하는 값의 색에 해당하는 값을 반환받고, 그 값 만큼 10의 제곱 수를 반환받음
 
-print((r1 + r2) * mul)
+print((r1 + r2) * mul) # 결과 출력
